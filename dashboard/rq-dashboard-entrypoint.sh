e13d9659c2eed0c2cc33d08612bfcd78474e616a
anri-c <asa.oki+git@gmail.com>
2016-10-13 12:09:30 +0900

    Changes to be committed: 	modified:   rq-dashboard-entrypoint.sh

--- dashboard/rq-dashboard-entrypoint.sh
+++ dashboard/rq-dashboard-entrypoint.sh
@@ -3,5 +3,5 @@
 /usr/bin/rq-dashboard -H ${DB_PORT_6379_TCP_ADDR}
 #
 # if use basic auth
-# /usr/bin/rq-dashboard --username <USERNAME> --password <PASSWORD> -H ${DB_PORT_6379_TCP}
+# /usr/bin/rq-dashboard --username <USERNAME> --password <PASSWORD> -H ${DB_PORT_6379_TCP_ADDR}
 #
